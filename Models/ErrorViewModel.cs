00aa611a1960b37ac56e63ba49e37c484766127d
jkrajnc <35113051+jkrajnc@users.noreply.github.com>
2018-01-07 11:23:27 +0100

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RIRIS10.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
